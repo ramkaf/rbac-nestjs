703a5cf74f9fbf57d0dc8aeeb2413b9aa59c01b6
ramkaf <raminkhaledianblog@gmail.com>
2026-07-04 19:11:12 +0330

    test

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { register } from '../controllers/auth.controller.js';
-
-const routes = express.Router();
-
-routes.use('/auth', register)
-
-export default routes
